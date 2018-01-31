7375d9daa8b69055cf1bb34751abb763f9a56fce
Felix von Poblotzki <xilaew@gmail.com>
2018-01-31 01:07:29 +0100

    improved Anmeldewebsite REST Service
    In order to become more flexible and to beter interact with JudoShiai the REST Service for Competitor Registration can now read Competition Details and Category Definitions from JudoShiai file

---
+++ Anmeldewebsite/lib.php
@@ -0,0 +1,124 @@
+<?php
+function sqlite_getInfo ($db) {
+	$result = new \stdClass;
+	$result->Competition = $db->querySingle("SELECT value FROM info WHERE item=='Competition'");
+	$result->Date = $db->querySingle("SELECT value FROM info WHERE item=='Date'");
+	$result->Place = $db->querySingle("SELECT value FROM info WHERE item=='Place'");
+	return $result;
+}
+
+function sqlite_getCategories ($db) {
+	$male = array();
+	$female = array();
+	$queryResults = $db->query("SELECT age, agetext, flags, weight, weighttext FROM catdef");
+	while ($row = $queryResults->fetchArray()) {
+		$map=&$female;
+		$sex="f";
+		if ($row["flags"]==1) {
+			$map=&$male;
+			$sex="m";
+		}
+		$age=$row[age];
+		if (($cat=$map[$age])!=NULL){
+			$cat->weights[]=$row[weight];
+			$cat->weightTexts[]=$row[weighttext];
+		} else {
+			$cat= new \stdClass;
+			$cat->age=$age;
+			$cat->agetext=$row[agetext];
+			$cat->sex=$sex;
+			$cat->weights=array($row[weight]);
+			$cat->weightTexts=array($row[weighttext]);
+			$map[$age]=$cat;
+		}
+	}
+	$result=new \stdClass;
+	$result->male = $male;
+	$result->female = $female;
+	return $result;
+}
+
+function csv_getCompetitors($input){
+  $result=array();
+  $indices=array("firstName","lastName","yearOfBirth","sex","club");
+  $f = fopen("data.csv", "r");
+  while (($line = fgetcsv($f)) !== false) {
+    $competitor=array();
+    foreach($line as $k => $content){
+      $key=$indices[$k];
+      $competitor[$key]=$content;
+    }
+    $result[]=$competitor;
+  }
+  fclose($f);
+  echo json_encode($result);
+}
+
+function csv_addCompetitor($competitor,$fp) {
+  $indices=array("firstName","lastName","yearOfBirth","sex","weight","category","club");
+  $result= new \stdClass;
+  if ($competitor->lastName=="" and $competitor->firstName=="" ) {
+    $result->msg="Please enter a name!";
+  } elseif ($competitor->club=="" ) {
+    $result->msg="Please enter a valid club!";
+  } elseif ($competitor->sex!="m" and $competitor->sex!="f" ) {
+    $result->msg="Transgenders not allowed!";
+  }elseif ($competitor->yearOfBirth>2006 or $competitor->yearOfBirth<1998 ) {
+    $result->msg="Year Of Birth must be between 1998 and 2006";
+  } else {
+    $competitor->category=map_category($competitor->sex,$competitor->yearOfBirth,$competitor->weight);
+    $line=array();
+    foreach ($indices as $key){
+      $line[]=$competitor->$key;
+      $result->$key=$competitor->$key;
+    }
+    if (!fputcsv($fp,$line)) {
+      $result->msg="Not saved due to internal error!";
+    } 
+  }
+  return $result;
+}
+
+function map_category ($sex, $yearOfBirth, $weight ) {
+  $mU15=array("-34", "-37", "-40", "-43", "-46", "-50", "-55", "-60", "-66", "+66");
+  $fU15=array("-33", "-36", "-40", "-44", "-48", "-52", "-57", "-63", "+63");
+  $mU18=array("-43", "-46", "-50", "-55", "-60", "-66", "-73", "-81", "-90", "+90");
+  $fU18=array("-40", "-44", "-48", "-52", "-57", "-63", "-70", "-78", "+78");
+  $mU21=array("-55", "-60", "-66", "-73", "-81", "-90", "-100", "+100");
+  $fU21=array("-44", "-48", "-52", "-57", "-63", "-70", "-78", "+78");
+  $category="";
+  $weights=array();
+  if ($yearOfBirth <= 2006 and $yearOfBirth >= 2004 ){
+    if ( $sex == "m"){
+      $category="männliche Jugend U15";
+      $weights=$mU15;
+    } else if ( $sex == "f"){
+      $category="weibliche Jugend U15";
+      $weights=$fU15;
+    }
+  } else if ($yearOfBirth <= 2003 and $yearOfBirth >= 2001 ){
+    if ( $sex == "m"){
+      $category="Männer U18";
+      $weights=$mU18;
+    } else if ( $sex == "f"){
+      $category="Frauen U18";
+      $weights=$fU18;
+    }
+  } else if($yearOfBirth <= 2000 and $yearOfBirth >= 1998 ){
+    if ( $sex == "m"){
+      $category="Männer U21";
+      $weights=$mU21;
+    } else if ( $sex == "f"){
+      $category="Frauen U21";
+      $weights=$fU21;
+    }
+  };
+  if ( in_array($weight, $weights) and $category!=""){
+    $category .= " ".$weight."kg";
+  } else {
+    $category = "";
+  }
+  return $category;
+}
+
+?>
--- Anmeldewebsite/rest.php
+++ Anmeldewebsite/rest.php
@@ -1,4 +1,6 @@
 <?php
+require 'lib.php';
+
 $categories = new \stdClass;
 $categories->mU15= new \stdClass;
 $categories->mU15->maxAge=14;
@@ -18,13 +20,14 @@ $categories->fU21=array("-44", "-48", "-52", "-57", "-63", "-70", "-78", "+78");
 header("Content-Type:application/json");
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'));
 session_start();
 
 // create SQL based on HTTP method
 switch ($method) {
   case 'GET':
-    readJudoShiai();
+    get($request);
 #    echo json_encode($categories);
 #    methodNotAllowed();
   break;
@@ -41,29 +44,34 @@ switch ($method) {
     methodNotAllowed();
 }
 
-function get($input){
-  $result=array();
-  $indices=array("firstName","lastName","yearOfBirth","sex","club");
-  $f = fopen("data.csv", "r");
-  while (($line = fgetcsv($f)) !== false) {
-    $competitor=array();
-    foreach($line as $k => $content){
-      $key=$indices[$k];
-      $competitor[$key]=$content;
-    }
-    $result[]=$competitor;
-  }
-  fclose($f);
-  echo json_encode($result);
+function get($request) {
+	$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
+	switch ($table) {
+		case 'info':
+		get_info();
+		break;
+		case 'categories':
+		get_categories();
+		break;
+	}
 }
 
-function post($input){
+function get_info(){
+	echo json_encode(sqlite_getInfo(new SQLite3('template.shi')));
+}
+
+function get_categories(){
+	echo json_encode(sqlite_getCategories(new SQLite3('template.shi')));
+}
+
+
+function post_competitors($input){
   $fp = fopen("data.csv", "a"); // $fp is now the file pointer to file $filename
   if (!$input){
     http_response_code(400);
     $msg="Input could not be parsed as JSON.";
   } elseif ( is_object($input) ) {
-    $result = add_competitor($input,$fp);
+    $result = csv_addCompetitor($input,$fp);
     if ($result->msg) {
       http_response_code(200);
     } else {
@@ -72,7 +80,7 @@ function post($input){
     }
   } elseif ( is_array($input) ) {
     foreach( $input as $competitor ){
-      add_competitor($competitor,$fp);
+      csv_addCompetitor($competitor,$fp);
     }
   } else {
     http_response_code(400);
@@ -87,121 +95,5 @@ function post($input){
 
 function methodNotAllowed(){
     http_response_code(405);
-}
-
-function add_competitor($competitor,$fp) {
-  $indices=array("firstName","lastName","yearOfBirth","sex","weight","category","club");
-  $result= new \stdClass;
-  if ($competitor->lastName=="" and $competitor->firstName=="" ) {
-    $result->msg="Please enter a name!";
-  } elseif ($competitor->club=="" ) {
-    $result->msg="Please enter a valid club!";
-  } elseif ($competitor->sex!="m" and $competitor->sex!="f" ) {
-    $result->msg="Transgenders not allowed!";
-  }elseif ($competitor->yearOfBirth>2006 or $competitor->yearOfBirth<1998 ) {
-    $result->msg="Year Of Birth must be between 1998 and 2006";
-  } else {
-    $competitor->category=map_category($competitor->sex,$competitor->yearOfBirth,$competitor->weight);
-    $line=array();
-    foreach ($indices as $key){
-      $line[]=$competitor->$key;
-      $result->$key=$competitor->$key;
-    }
-    if (!fputcsv($fp,$line)) {
-      $result->msg="Not saved due to internal error!";
-    } 
-  }
-  return $result;
-}
-
-function map_category ($sex, $yearOfBirth, $weight ) {
-  $mU15=array("-34", "-37", "-40", "-43", "-46", "-50", "-55", "-60", "-66", "+66");
-  $fU15=array("-33", "-36", "-40", "-44", "-48", "-52", "-57", "-63", "+63");
-  $mU18=array("-43", "-46", "-50", "-55", "-60", "-66", "-73", "-81", "-90", "+90");
-  $fU18=array("-40", "-44", "-48", "-52", "-57", "-63", "-70", "-78", "+78");
-  $mU21=array("-55", "-60", "-66", "-73", "-81", "-90", "-100", "+100");
-  $fU21=array("-44", "-48", "-52", "-57", "-63", "-70", "-78", "+78");
-  $category="";
-  $weights=array();
-  if ($yearOfBirth <= 2006 and $yearOfBirth >= 2004 ){
-    if ( $sex == "m"){
-      $category="männliche Jugend U15";
-      $weights=$mU15;
-    } else if ( $sex == "f"){
-      $category="weibliche Jugend U15";
-      $weights=$fU15;
-    }
-  } else if ($yearOfBirth <= 2003 and $yearOfBirth >= 2001 ){
-    if ( $sex == "m"){
-      $category="Männer U18";
-      $weights=$mU18;
-    } else if ( $sex == "f"){
-      $category="Frauen U18";
-      $weights=$fU18;
-    }
-  } else if($yearOfBirth <= 2000 and $yearOfBirth >= 1998 ){
-    if ( $sex == "m"){
-      $category="Männer U21";
-      $weights=$mU21;
-    } else if ( $sex == "f"){
-      $category="Frauen U21";
-      $weights=$fU21;
-    }
-  };
-  if ( in_array($weight, $weights) and $category!=""){
-    $category .= " ".$weight."kg";
-  } else {
-    $category = "";
-  }
-  return $category;
-}
-
-function readJudoShiai () {
-	$db= new SQLite3('template.shi');
-	echo $db->querySingle("SELECT value FROM info WHERE item=='Competition'");
-	
-	#$db = sqlite_open('template.shi', 0666, $sqliteError);
-	#$result = sqlite_query("SELECT value FROM info WHERE item=='Competition'",$db);
-	#echo sqlite_fetch_single($result);
-	}
-
-#function add_competitor($competitor,$fp) {
-#  $reslt= new \stdClass;
-#  if ($competitor->lastName=="" and $competitor->firstName=="" ) {
-#    $result->msg="Please enter a name!";
-#  } elseif ($competitor["club"]=="" ) {
-#    $result->msg="Please enter a valid club!";
-#  } elseif ($competitor["sex"]!="m" and $competitor["sex"]!="f" ) {
-#    $result->msg="Transgenders not allowed!";
-#  } else {
-#    $line=array();
-#    foreach ($indices as $key){
-#      $line[]=$competitor[$key];
-#    }
-#    if (fputcsv($fp,$line)) {
-#      $result->msg="success!!";
-#    } else {
-#    }
-#  }
-#  return $result;
-#}
-
-
-function maybelater(){// die if SQL statement failed
-  if (!$result) {
-    http_response_code(404);
-    die(mysqli_error());
-  }
-   
-  // print results, insert id or affected row count
-  if ($method == 'GET') {
-    if (!$key) echo '[';
-    for ($i=0;$i<mysqli_num_rows($result);$i++) {
-      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-    }
-    if (!$key) echo ']';
-  } elseif ($method == 'POST') {
-  } else {
-  }
 }
 ?>
