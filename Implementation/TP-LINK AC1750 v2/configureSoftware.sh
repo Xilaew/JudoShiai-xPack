4bf0f7bd2a70a21627a86f41ae1ce91dfbe1a9b7
Felix von Poblotzki <xilaew@gmail.com>
2018-11-03 14:21:39 +0100

    Renamed folders and cleaned old stuff

--- Implementation/TP-LINK AC1750 v2/configureSoftware.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'judoshiai:*:100:100:judoshiai:/var/turnierinfo:/bin/false' >> /etc/passwd
-echo 'judoshiai::0:0:99999:7:::' >> /etc/shadow
-
-chmod 755 /etc/init.d/turnierinfo
-/etc/init.d/turnierinfo enable
-reboot
--- Implementation/TP-LINK AC1750 v2/installSoftware.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-opkg update
-
-# FTP Server
-opkg install vsftpd
